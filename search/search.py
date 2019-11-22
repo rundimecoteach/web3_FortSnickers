7646de9abbf1a3d482bef001654722452484666e
Palavor <palavor.jeu@gmail.com>
2019-11-22 15:39:40 +0100

    corection lien

--- search/search.py
+++ search/search.py
@@ -1,6 +1,6 @@
 import json
 
-with open('/home/jessy/Documents/web3.0/web3_FortSnickers/search/tournois.json') as json_data:
+with open('search/tournois.json') as json_data:
     tournois = json.load(json_data)
     mdpe = raw_input("Entrez le numero du tournois ")
     print(tournois[mdpe])
